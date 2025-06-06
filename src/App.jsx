62974f01758b5d8018542a35e1557809d3b80f0b
greeif <griffiths.alex@gmail.com>
2025-06-06 11:26:20 +0100

    changes to make app render

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { comments } from "./commentData";
 import Card from "./Card";
 
-const commentItems = comments.map((comment) => (
-  <Card commentObject={comment} />
+const commentItems = comments.map((comment, index) => (
+  <Card key={index} commentObject={comment} />
 ));
+
 function App(props) {
   return <div>{commentItems}</div>;
 }
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 function Body (props) {
   return <p>{props.comment}</p>;
